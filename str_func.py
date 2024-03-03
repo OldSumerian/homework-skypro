fc8d470b27cd8a8050f6b6262a0947581b865337
Sumerian <shemerov.s@yandex.ru>
2024-03-03 18:17:13 +0300

    add new to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def make_upper_case(string):
+    """
+    Function to make uppercase text
+    :param string:
+    :return:
+    """
     return string.upper()
+
+
+def make_capitalize(string):
+    """
+    Function to make capitalize text
+    :param string:
+    :return:
+    """
+    return string.capitalize()
